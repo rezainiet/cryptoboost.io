6267e06a601bc80756d72524754ae5e8d400c348
rezainiet <masudrezaog@gmail.com>
2025-08-11 13:11:27 +0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="gradient-bg flex min-h-screen items-center justify-center from-blue-500 to-purple-600 p-8">
+      <h1 className="text-2xl">CryptoBoost</h1>
+    </div>
+  );
+}
+
+export default App;
